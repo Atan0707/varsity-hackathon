7a19de3e51c4f996cc20475f64f2f2f595761f07
Hariz Hakim <133663670+Atan0707@users.noreply.github.com>
2025-03-24 03:08:13 +0800

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,1 +1,1 @@
-export const WALLETCONNECT_PROJECT_ID = ""; // Add your WalletConnect project ID here
+export const WALLETCONNECT_PROJECT_ID = "05d066d5e67c69d7f84c8bcc1e812bd5"; // Add your WalletConnect project ID here
