bab6d598ada7063c931d38df7c38c095e775890f
tgxzd <aizaddtengkuazman@gmail.com>
2025-03-25 01:10:39 +0800

    add text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { BlurFadeTextDemo } from "@/components/demo/blur-fade-text-demo"
+
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      {/* Page is empty as requested */}
+    <div className="min-h-screen flex items-center justify-start pl-16 md:pl-24 lg:pl-32">
+      <BlurFadeTextDemo />
     </div>
   );
 }
